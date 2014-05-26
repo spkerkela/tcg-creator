547babe406511418cea0b8bc446680d9074a6f14
Simo-Pekka Kerkelä <simopekka1990@gmail.com>
2014-05-26 23:27:02 +0300

    add controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TcgCreator::Application.routes.draw do
+  get "users/new"
   get "static_pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -6,6 +7,7 @@ TcgCreator::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
+  match '/signup', to: 'users#new', via: 'get'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
